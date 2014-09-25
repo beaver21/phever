491779ee427d60412e182b3fb8f46b76f2bfd224
Marc-Olivier Fiset <marco.fiset@gmail.com>
2014-09-25 13:03:54 -0400

    Added basic methods for initial status

--- spec/Phever/EventSourcedSpec.php
+++ spec/Phever/EventSourcedSpec.php
@@ -11,4 +11,26 @@ class EventSourcedSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Phever\EventSourced');
     }
+
+    function it_should_have_a_version_number()
+    {
+        $this->getVersion()->shouldBe(0);
+    }
+
+    function it_should_have_no_events()
+    {
+        $this->getEvents()->shouldHaveCount(0);
+    }
+
+    function it_should_have_a_guid_as_an_id()
+    {
+        $this->getId()->shouldNotBe(null);
+    }
+
+    function it_can_be_instantiated_with_a_given_id()
+    {
+        $this->beConstructedWith(1);
+
+        $this->getId()->shouldBe(1);
+    }
 }
--- src/Phever/EventSourced.php
+++ src/Phever/EventSourced.php
@@ -4,4 +4,25 @@ namespace Phever;
 
 class EventSourced
 {
+    private $id;
+
+    public function __construct($id = null)
+    {
+        $this->id = $id ?: guid();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getVersion()
+    {
+        return 0;
+    }
+
+    public function getEvents()
+    {
+        return [];
+    }
 }
